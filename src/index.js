081eba41ddc605331ec8020b7ea3bbcacf2cc7b0
youdai.fu <fuyoudai1@bngrp.com>
2021-04-07 15:14:54 +0800

    调试react源码

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./index.css";
+import jsx from "./pages/ExamplePage";
+
+ReactDOM.render(jsx, document.getElementById("root"));
+console.log("React", React.version); //sy-log
